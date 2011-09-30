26b9cd23ece975604015b75da64f81bf08245e71
Brandonian <brandon@brandonmorrison.com>
2011-09-30 17:36:57 -0400

    Much more work done. Fleshed out dropdown widget more. Added better polygon support. Still not working

--- gmap_input.js
+++ gmap_input.js
@@ -8,6 +8,8 @@
   var DRAW_POLY = 'draw polygon';
   var DRAW_BOUNDS = 'draw bounds';
   
+  var FEATURE_COUNT_UNLIMITED = -1;
+  
   // 'Global' states
   var mapState = MAP_STATE_PANNING;
   var drawType = DRAW_POINT;
@@ -17,9 +19,11 @@
       'lat': 41.879535,
       'lon': -87.624333,
       'zoom': 7
-    }
+    },
+    imagePath: 'img',
+    featureMaxCount: FEATURE_COUNT_UNLIMITED
   };
-  
+
   $.fn.gmapInput = function(optionOverrides) {
     var items = $(this);
 
@@ -42,15 +46,12 @@
       };
 
       map = new google.maps.Map(document.getElementById(map_id), myOptions);
-      
-      var drawControlContainer = document.createElement('DIV');
-      var drawControl = $('<div>').text('test');
-      drawControlContainer.innerHTML = drawControl.html();
-      map.controls[google.maps.ControlPosition.TOP_RIGHT].push(drawControlContainer);
-      
+
+      generateControl(map);
+
       var polyOptions = {
-        strokeColor: '#000000',
-        fillColor: '#333333',
+        strokeColor: '#FFCC66',
+        fillColor: '#FFCC66',
         strokeOpacity: 1.0,
         strokeWeight: 3
       };
@@ -64,13 +65,54 @@
         
         path.push(event.latLng);
         
+        var handle = new google.maps.MarkerImage(options.imagePath + '/point-handle.png',
+          new google.maps.Size(15, 15),
+          new google.maps.Point(0, 0),
+          new google.maps.Point(7.5, 7.5)
+        );
+
         var marker = new google.maps.Marker({
           position: event.latLng,
           title: '#' + path.getLength(),
-          map: map
+          map: map,
+          icon: handle
         });
       }
     });
   };
+
+  /**
+   * Generates the dropdown widget.
+   */
+
+  function generateControl(map) {
+    var drawControlContainer = document.createElement('DIV');
+    var list = $('<ul>').css({
+      'background-color': '#FFF',
+      'list-style': 'none',
+      'padding-left': 0
+    })
+      .html('<li>Draw Point</li><li>Draw Line</li><li>Draw Polygon</li><li>Draw Bounds</li>');
+    
+    var drawControl = $('<div>').append(list);
+    drawControlContainer.innerHTML = drawControl.html();
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(drawControlContainer);
+    
+    $('li', drawControlContainer).click(function() {
+      // @TODO: Toggle drawing status here.
+      alert("HI");
+    });
+  }
+
+  var widgetProcessors = new Object();
+
+  function runFunction(name, arguments)
+  {
+    var fn = window[name];
+    if(typeof fn !== 'function')
+      return;
+
+    fn.apply(window, arguments);
+  }
 
 })( jQuery );
