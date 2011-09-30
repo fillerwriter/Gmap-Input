0cb2acec16887616a869e9a59d5540647a8b1336
Brandonian <brandon@brandonmorrison.com>
2011-09-30 11:07:21 -0400

    Added ability to pass in start coordinates. Initial polygon mapping code

--- gmap_input.js
+++ gmap_input.js
@@ -12,18 +12,32 @@
   var mapState = MAP_STATE_PANNING;
   var drawType = DRAW_POINT;
   
-  $.fn.gmapInput = function() {
+  var optionDefaults = {
+    startPoint: {
+      'lat': 41.879535,
+      'lon': -87.624333,
+      'zoom': 7
+    }
+  };
+  
+  $.fn.gmapInput = function(optionOverrides) {
     var items = $(this);
-    
+
     items.each(function(index, element) {
+      var options = optionDefaults;
+      
+      if (optionOverrides) {
+        $.extend(options, optionOverrides);
+      }
+
       var map_id = 'mapinput_' + index;
       var map_container = $('<div>').attr('id', map_id).attr('style', 'width: 500px; height: 500px');
       $(element).after(map_container);
       
-      var chicago = new google.maps.LatLng(41.879535, -87.624333);
+      var start = new google.maps.LatLng(options.startPoint.lat, options.startPoint.lon);
       var myOptions = {
-        zoom: 7,
-        center: chicago,
+        zoom: options.startPoint.zoom,
+        center: start,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
 
@@ -33,6 +47,30 @@
       var drawControl = $('<div>').text('test');
       drawControlContainer.innerHTML = drawControl.html();
       map.controls[google.maps.ControlPosition.TOP_RIGHT].push(drawControlContainer);
+      
+      var polyOptions = {
+        strokeColor: '#000000',
+        fillColor: '#333333',
+        strokeOpacity: 1.0,
+        strokeWeight: 3
+      };
+      poly = new google.maps.Polygon(polyOptions);
+      poly.setMap(map);
+      
+      google.maps.event.addListener(map, 'click', addLatLng);
+      
+      function addLatLng(event) {
+        var path = poly.getPath();
+        
+        path.push(event.latLng);
+        
+        var marker = new google.maps.Marker({
+          position: event.latLng,
+          title: '#' + path.getLength(),
+          map: map
+        });
+      }
     });
   };
+
 })( jQuery );
