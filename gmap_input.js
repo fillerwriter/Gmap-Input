5cb4d57608956e7666c4fd384e06d9081bebdb10
Brandonian <brandon@brandonmorrison.com>
2011-10-04 16:47:37 -0400

    More tweaks. It outputs WKT now! (for polygons)

--- gmap_input.js
+++ gmap_input.js
@@ -19,6 +19,8 @@
     imagePath: 'img',
     featureMaxCount: FEATURE_COUNT_UNLIMITED
   };
+  
+  var dblClickTimer = undefined;
 
   $.fn.gmapInput = function(optionOverrides) {
     var items = $(this);
@@ -38,13 +40,14 @@
       var myOptions = {
         zoom: options.startPoint.zoom,
         center: start,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDoubleClickZoom: true
       };
 
       map = new google.maps.Map(document.getElementById(map_id), myOptions);
 
       // Generates control
-      generateControl(map);
+      generateControl(map, options);
 
       // 'Global' states
       var mapState = MAP_STATE_PANNING;
@@ -61,90 +64,181 @@
       poly.setMap(map);
 
       google.maps.event.addListener(map, 'click', mapClickHandler);
-      google.maps.event.addListener(map, 'doubleclick', mapDblClickHandler)
+      google.maps.event.addListener(map, 'dblclick', mapDblClickHandler);
+      google.maps.event.addListener(map, 'rightclick', mapDblClickHandler);
+      $(document).keydown(function(event) {
+        var data = $('.control').data();
+        if (event.which == 46 && data.currentDrawObject != undefined) {
+          data.currentDrawObject.setMap(null);
+          data.currentDrawObject = undefined;
+        }
+      });
     });
   };
 
   function mapClickHandler(event) {
+    dblClickTimer = setTimeout(function() {mapClickHandlerPostTimer(event);}, 200);
+  }
+
+  function mapClickHandlerPostTimer(event) {
     var data = $('.control').data();
     if (data.mapState == MAP_STATE_DRAWING) {
-      widgetProcessors.polygon.click(event);
+      switch (data.drawType) {
+        case DRAW_POLY:
+          widgetProcessors.polygon.mapclick(event, data);
+        break;
+      }
     }
   }
 
   function mapDblClickHandler(event) {
-  
+    clearTimeout(dblClickTimer);
+    var data = $('.control').data();
+    widgetProcessors.polygon.mapdoubleclick(event, data);
   }
 
   /**
    * Generates the dropdown widget.
    */
 
-  function generateControl(map) {
+  function generateControl(map, options) {
     var drawControlContainer = document.createElement('DIV');
     var list = $('<ul>').addClass('control').css({
       'background-color': '#FFF',
       'list-style': 'none',
-      'padding-left': 0
+      'padding-left': 0,
+      'font-family': '"Helvetica", sans-serif',
+      'font-size': '0.8em'
     })
       .html('<li>Draw Point</li><li>Draw Line</li><li>Draw Polygon</li><li>Draw Bounds</li>');
 
-    var drawControl = $('<div>').append(list);
+    var drawControl = $('<div>').append(list).append('<div class="dropdown">expand</div>');
+    
     drawControlContainer.innerHTML = drawControl.html();
     map.controls[google.maps.ControlPosition.TOP_RIGHT].push(drawControlContainer);
 
+    // Data structure happens here.
+    /**
+     * Data structures.
+     *  - value
+     *    - [
+     *      'type': (point/line/poly)
+     *      'points': [
+     *        (array of objects, lat/lon)
+     *      ],
+     *    ]
+     */
     $('.control', drawControlContainer).data({
       'mapState': MAP_STATE_PANNING,
       'drawType': DRAW_POINT,
-      'value': []
+      'value': [], // Internal data.
+      'currentDrawObject': undefined,
+      'currentValueID': 0, // which item in value are we targeting at the moment?
+      'options': options
     });
+
     $('li', drawControlContainer).click(function() {
       var data = $(this).parents('.control').data();
+      
+      $(this).parents('.control').find('li').css('background-color', '#FFF');
+
+      switch ($(this).text()) {
+        case 'Draw Point':
+          data.drawType = DRAW_POINT;
+        break;
+        case 'Draw Line':
+          data.drawType = DRAW_LINE;
+        break;
+        case 'Draw Polygon':
+          data.drawType = DRAW_POLY;
+        break;
+        case 'Draw Bounds':
+          data.drawType = DRAW_BOUNDS;
+        break;
+      }
+      
       if (data.mapState == MAP_STATE_PANNING) {
         data.mapState = MAP_STATE_DRAWING;
+        $(this).css('background-color', '#F00');
       }
       else {
         data.mapState = MAP_STATE_PANNING;
       }
-      alert(data.mapState);
     });
   }
 
+  /**
+   * Widget Processor object holds all the code for handling click events for our map.
+   *
+   * Features have many different event handlers
+   *  - mapclick
+   *  - mapdoubleclick
+   *  - featureclick
+   **/
   var widgetProcessors = new Object();
 
   widgetProcessors.polygon = {
-    click: function(event) {
-      alert("POLYGON CLICK");
+    // click
+    mapclick: function(event, data) {
+      if (data.currentDrawObject == undefined) {
+        var polyOptions = {
+          strokeColor: '#FFCC66',
+          fillColor: '#FFCC66',
+          strokeOpacity: 1.0,
+          strokeWeight: 3
+        };
+
+        data.currentDrawObject = new google.maps.Polygon(polyOptions);
+        data.currentDrawObject.setMap(map);
+
+        data.currentValueID = data.value.length;
+
+        data.value[data.currentValueID] = {
+          type: 'polygon',
+          points: new Array()
+        }
+        
+        
+      }
+      var path = data.currentDrawObject.getPath();
+      path.push(event.latLng);
+
+      data.value[data.currentValueID].points.push({
+        lat: event.latLng.lat(),
+        lon: event.latLng.lng()
+      });
+      
+      $('textarea').val(outputWKT(data.value));
+    },
+    // Doubleclick
+    mapdoubleclick: function (event, data) {
+      if (data.currentDrawObject != undefined) {
+        google.maps.event.addListener(data.currentDrawObject, 'click', function(event) {
+          widgetProcessors.polygon.featureclick(event, data);
+        });
+        data.currentDrawObject = undefined;
+      }
+    },
+    featureclick: function (event, data) {
+      widgetProcessors.polygon.mapclick(event, data);
     }
   };
-
-  // @TODO: Add to widget Processors
-  function addLatLng(event) {
-    var path = poly.getPath();
-
-    path.push(event.latLng);
-
-    var handle = new google.maps.MarkerImage(options.imagePath + '/point-handle.png',
-      new google.maps.Size(15, 15),
-      new google.maps.Point(0, 0),
-      new google.maps.Point(7.5, 7.5)
-    );
-
-    var marker = new google.maps.Marker({
-      position: event.latLng,
-      title: '#' + path.getLength(),
-      map: map,
-      icon: handle
-    });
+  
+  function outputWKT(data) {
+    var output = '';
+    for (i in data) {
+      switch (data[i]['type']) {
+        case 'polygon':
+          var segment = new Array();
+          output += 'POLYGON ((';
+          for (j in data[i].points) {
+            segment.push(data[i].points[j].lat + ' ' + data[i].points[j]['lon']);
+          }
+          output += segment.toString();
+          output += '))';
+        break;
+      }
+    }
+    return output;
   }
-
-  function runFunction(name, arguments)
-  {
-    var fn = window[name];
-    if(typeof fn !== 'function')
-      return;
-
-    fn.apply(window, arguments);
-  }
-
 })( jQuery );
