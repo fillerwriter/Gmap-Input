f327c807d6af52bfa835bd5397c6682f8f5a4786
Brandonian <brandon@brandonmorrison.com>
2011-10-18 17:05:14 -0400

    Complete rewrite. Much more stable base

--- gmap_input.js
+++ gmap_input.js
@@ -1,4 +1,15 @@
-(function( $ ){
+/*!
+ * jQuery lightweight plugin boilerplate
+ * Original author: @ajpiano
+ * Further changes, comments: @addyosmani
+ * Licensed under the MIT license
+ */
+
+// the semi-colon before the function invocation is a safety
+// net against concatenated scripts and/or other plugins
+// that are not closed properly.
+;(function ( $, window, document, undefined ) {
+
   // Constants
   var MAP_STATE_PANNING = 'panning';
   var MAP_STATE_DRAWING = 'drawing';
@@ -10,14 +21,203 @@
   
   var FEATURE_COUNT_UNLIMITED = -1;
 
+  // undefined is used here as the undefined global
+  // variable in ECMAScript 3 and is mutable (i.e. it can
+  // be changed by someone else). undefined isn't really
+  // being passed in so we can ensure that its value is
+  // truly undefined. In ES5, undefined can no longer be
+  // modified.
+
+  // window and document are passed through as local
+  // variables rather than as globals, because this (slightly)
+  // quickens the resolution process and can be more
+  // efficiently minified (especially when both are
+  // regularly referenced in your plugin).
+
+  // Create the defaults once
+  var pluginName = 'gmapInput',
+      defaults = {
+        startPoint: {
+          'lat': 41.879535,
+          'lon': -87.624333,
+          'zoom': 7
+        },
+        imagePath: 'img',
+        featureMaxCount: FEATURE_COUNT_UNLIMITED
+      };
+
+  // The actual plugin constructor
+  function GmapInput( element, options ) {
+      this.element = element;
+
+    // jQuery has an extend method that merges the
+    // contents of two or more objects, storing the
+    // result in the first object. The first object
+    // is generally empty because we don't want to alter
+    // the default options for future instances of the plugin
+    this.options = $.extend( {}, defaults, options) ;
+
+    this._defaults = defaults;
+    this._name = pluginName;
+    this._map = null;
+
+    this.init();
+  }
+
+  GmapInput.prototype.init = function () {
+    this._mapcontainer = $(this.element).after('<div class="gmapInputMap"></div>').siblings('.gmapInputMap').get(0);
+    var start = new google.maps.LatLng(this.options.startPoint.lat, this.options.startPoint.lon);
+    var mapOptions = {
+      zoom: this.options.startPoint.zoom,
+      center: start,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDoubleClickZoom: true
+    };
+
+    var polyOptions = {
+      strokeColor: '#FFCC66',
+      fillColor: '#FFCC66',
+      strokeOpacity: 1.0,
+      strokeWeight: 3
+    };
+
+    this._map = new google.maps.Map(this._mapcontainer, mapOptions);
+    google.maps.event.addListener(this._map, 'click', function(e) {
+      var path = poly.getPath();
+      path.push(e.latLng);
+    });
+
+    poly = new google.maps.Polygon(polyOptions);
+    poly.setMap(this._map);
+
+    var drawControlContainer = document.createElement('DIV');
+    var list = $('<ul>').addClass('control').css({
+      'background-color': '#FFF',
+      'list-style': 'none',
+      'padding-left': 0,
+      'font-family': '"Helvetica", sans-serif',
+      'font-size': '0.8em'
+    })
+      .html('<li>Draw Point</li><li>Draw Line</li><li>Draw Polygon</li><li>Draw Bounds</li>');
+
+    var drawControl = $('<div>').append(list).append('<div class="dropdown">expand</div>');
+    
+    drawControlContainer.innerHTML = drawControl.html();
+    this._map.controls[google.maps.ControlPosition.TOP_RIGHT].push(drawControlContainer);
+
+
+    // TMP TEST INFO
+    var internalData = new GmapJSON();
+    internalData.addFeature('Polygon');
+    internalData.addCoordinate(40, 20);
+    internalData.addCoordinate(50, 30);
+    internalData.addFeature('Point');
+    internalData.addCoordinate(30, 50);
+    $(this.element).val(internalData.stringify());
+  };
+
+  // Returns map object.
+  GmapInput.prototype.getMap = function () {
+    return this._map;
+  }
+
+  // General click callback.
+  GmapInput.prototype.click = function () {
+    
+  }
+
+  // General doubleclick callback.
+  GmapInput.prototype.doubleclick = function () {
+    
+  }
+
+  // Switch drawing setting to polygon drawing
+  GmapInput.prototype.startPolygon = function () {
+    
+  }
+
+
+
+  // Data object. Handles manipulation of internal data.
+  function GmapJSON(options) {
+    this.data = [];
+    
+    this._currentFeature = -1;
+    this.init();
+  }
+  
+  // init function
+  GmapJSON.prototype.init = function() {
+  }
+
+  // add feature
+  GmapJSON.prototype.addFeature = function(featureType) {
+    this._currentFeature++;
+    this.data[this._currentFeature] = {
+      "type": featureType,
+      "coordinates": []
+    };
+    return this._currentFeature;
+  }
+  
+  // remove feature
+  GmapJSON.prototype.removeFeature = function (featurePos) {
+    if (this.data[featurePos] != undefined) {
+      this.data.splice(featurePos, 1);
+      return true;
+    }
+    return false;
+  }
+
+  // set current feature
+  GmapJSON.prototype.setCurrentFeature = function (featurePos) {
+    if (this.data[featurePos] != undefined) {
+      this._currentFeature = featurePos;
+      return true;
+    }
+    return false;
+  }
+
+  // add coordinate
+  GmapJSON.prototype.addCoordinate = function(lat, lon, featurePos) {
+    if (featurePos == undefined) {
+      featurePos = this._currentFeature;
+    }
+    
+    this.data[featurePos].coordinates.push([lat, lon]);
+  }
+  
+  // remove coordinate
+  GmapJSON.prototype.removeCoordinate = function(featurePos, position) {
+  
+  }
+
+  // Return current feature's coordinates
+  GmapJSON.prototype.currentFeature = function() {
+    return this.data[this._currentFeature]["coordinates"];
+  }
+
+  // stringify. Returns a string based on internal data.
+  GmapJSON.prototype.stringify = function() {
+    return JSON.stringify(this.data);
+  }
+
+  // A really lightweight plugin wrapper around the constructor,
+  // preventing against multiple instantiations
+  $.fn[pluginName] = function ( options ) {
+    return this.each(function () {
+      if (!$.data(this, 'plugin_' + pluginName)) {
+        $.data(this, 'plugin_' + pluginName,
+        new GmapInput( this, options ));
+      }
+    });
+  }
+
+})( jQuery, window, document );
+
+/*(function( $ ){
   var optionDefaults = {
-    startPoint: {
-      'lat': 41.879535,
-      'lon': -87.624333,
-      'zoom': 7
-    },
-    imagePath: 'img',
-    featureMaxCount: FEATURE_COUNT_UNLIMITED
+    
   };
   
   var dblClickTimer = undefined;
@@ -99,7 +299,7 @@
 
   /**
    * Generates the dropdown widget.
-   */
+   *
 
   function generateControl(map, options) {
     var drawControlContainer = document.createElement('DIV');
@@ -127,7 +327,7 @@
      *        (array of objects, lat/lon)
      *      ],
      *    ]
-     */
+     *
     $('.control', drawControlContainer).data({
       'mapState': MAP_STATE_PANNING,
       'drawType': DRAW_POINT,
@@ -177,7 +377,7 @@
    *  - mapclick
    *  - mapdoubleclick
    *  - featureclick
-   **/
+   **
   var widgetProcessors = new Object();
 
   widgetProcessors.polygon = {
@@ -223,4 +423,4 @@
     }
   };
 
-})( jQuery );
+})( jQuery );*/
